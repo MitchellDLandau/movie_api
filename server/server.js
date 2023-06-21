508502803e484512b80b1f484aa70b35e98d6df0
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-06-21 14:51:57 -0400

    Commit 1
    Created the documentation needed to start the project. Added log file, index, documentation, test, server.js. All of these allow me to create the server and request http and url responses.

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const http = require('http'),
+fs = require ('fs'),
+url = require ('url');
+
+// http.createServer((request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});           first server opened
+//   response.end('Hello Node!\n');
+// }).listen(8080);
+
+// console.log('My first Node test server is running on Port 8080.');
+
+
+
+
+// const fs = require('fs');
+
+// fs.readFile('input.text', (err, data) => {
+//     if (err) {
+//     throw err;
+// }
+// console.log('File content: ' + data.toString());
+// });
+
+
+
+
+http.createServer((request, response) => {      //http module used to set up the server. 
+    let addr = request.url;
+    q = url.parse(addr, true);
+    filePath = '';
+
+fs.appendFile('CareerFoundryTerminal/movie_api/log.txt', 'URL: ' + addr + '\nTimestamp:' + new Date() + '\n\n', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Added to log.');
+    }
+});
+
+if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');     //url module to grab and read a URL request sent by the user
+} else {
+    filePath = './CareerFoundryTerminal/movie_api/index.html';
+};
+
+fs.readFile(filePath, (err, data) => {          // fs module to send back the appropriate file.
+    if (err) {
+        throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+
+    response.write(data);
+    response.end();
+});
+}).listen(8080);
+console.log('My first Node test server is running on Port 8080.');
