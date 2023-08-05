3e21ad2abf21ac80ca81780c784e5c3114c6bd5d
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-08-05 15:15:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedT
 
 //getting all users information (ADMIN ONLY)
  app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
-    if (req.user.Auth !== true)
+    if (req.user.Auth != true)
     { 
         return res.status(400).send('Only moderators can use this function.');
     }
