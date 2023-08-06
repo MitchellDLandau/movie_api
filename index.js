6eb2f0e487336bb96a36630c97bd961c92ccc923
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-08-06 14:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedT
  app.get('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {
     if (req.user.Auth !== true)
     { 
-        console.log(req.user)
+        console.log(typeof(req.user.Auth))
         return res.status(400).send(req.user.Auth);
     }
     await Users.find()
