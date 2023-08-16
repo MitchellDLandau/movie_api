9cbe08668b227720f21e69ab744dc3c5c4ecaedc
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-08-16 14:02:40 -0400

    testing
    testing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
     origin: (origin, callback) => {
